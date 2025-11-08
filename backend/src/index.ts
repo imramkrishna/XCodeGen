6d01e1af3800747b9d60480fedab5ce7cf0b6069
Ram Krishna Yadav <itsramky234@gmail.com>
2025-11-08 23:55:31 +0545

    updated backend with new tsconfig

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,50 @@
 import dotenv from "dotenv"
-import Anthropic from '@anthropic-ai/sdk';
 import express from "express"
-import { TextBlock } from "@anthropic-ai/sdk/resources/messages";
-import { basePrompt as nodeBasePrompt } from "./defaults/node";
-import { basePrompt as reactBasePrompt } from "./defaults/react";
-import { BASE_PROMPT, getSystemPrompt } from "./prompts";
-import OpenAI from "openai";
+import { basePrompt as nodeBasePrompt } from "./defaults/node.js";
+import { basePrompt as reactBasePrompt } from "./defaults/react.js";
+import { BASE_PROMPT, getSystemPrompt } from "./prompts.js";
 import cors from "cors"
-import axios from "axios";
 import { GoogleGenAI } from "@google/genai";
-import { MessageFormat } from "./defaults/message";
-import { fullStackBasePrompt } from "./defaults/fullstack";
-import { chatMessage } from "./defaults/chatMessage";
+import { MessageFormat } from "./defaults/message.js";
+import { fullStackBasePrompt } from "./defaults/fullstack.js";
+import { chatMessage } from "./defaults/chatMessage.js";
 dotenv.config()
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const app = express();
 app.use(express.json())
-app.use(cors({
-    origin: ["https://x-code-gen.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}))
+// app.use(cors({
+//     origin: ["https://x-code-gen.vercel.app"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }))
+app.use(cors())
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 
 })
 app.post("/template", async (req, res) => {
     const prompt = req.body.prompt;
-    const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
-        contents: prompt + "Return either node,react or fullstack based on what do you think this project should be in.If it is both frontend and backend then return fullstack.Only return a single string word either 'node' or 'react' or 'fullstack'. Donot return anything extra.",
+
+    const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${process.env.GEMINI_API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "model": "qwen/qwen2.5-vl-32b-instruct:free",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt + "Return either node,react or fullstack based on what do you think this project should be in.If it is both frontend and backend then return fullstack.Only return a single string word either 'node' or 'react' or 'fullstack'. Donot return anything extra.",
+                }
+            ]
+        })
     });
-    let answer = response.text;
+    const data:any = await response.json();
+    const answer: string = (data?.choices?.[0]?.message?.content) ?? (data?.choices?.[0]?.content) ?? "";
+    console.log(data)
     if (answer?.trim() == "react") {
         res.json({
             prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.Make sure to make website responsive and beautiful as if it is made by one of the best ui ux designers.\n\n ${MessageFormat}\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
@@ -72,7 +84,7 @@ app.post("/chatmistral", async (req, res) => {
         const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
             method: "POST",
             headers: {
-                "Authorization": `Bearer ${process.env.MISTRAL_API_KEY}`,
+                "Authorization": `Bearer ${process.env.GEMINI_API_KEY}`,
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
@@ -80,7 +92,7 @@ app.post("/chatmistral", async (req, res) => {
                 "messages": [
                     {
                         "role": "user",
-                        "content": chatMessage+prompt
+                        "content": chatMessage + prompt
                     }
                 ]
             })
@@ -110,7 +122,7 @@ app.post("/chatqwen", async (req, res) => {
         const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
             method: "POST",
             headers: {
-                "Authorization": `Bearer ${process.env.MISTRAL_API_KEY}`,
+                "Authorization": `Bearer ${process.env.GEMINI_API_KEY}`,
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
@@ -148,7 +160,7 @@ app.post("/chatllama", async (req, res) => {
         const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
             method: "POST",
             headers: {
-                "Authorization": `Bearer ${process.env.MISTRAL_API_KEY}`,
+                "Authorization": `Bearer ${process.env.GEMINI_API_KEY}`,
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
@@ -200,7 +212,7 @@ app.post("/chatgemini", async (req, res) => {
         })
     }
 })
-app.post("/chatmode",async (req,res)=>{
+app.post("/chatmode", async (req, res) => {
     try {
         const messages = req.body.messages
 
@@ -212,7 +224,7 @@ app.post("/chatmode",async (req,res)=>{
         const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
             method: "POST",
             headers: {
-                "Authorization": `Bearer ${process.env.MISTRAL_API_KEY}`,
+                "Authorization": `Bearer ${process.env.GEMINI_API_KEY}`,
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
@@ -223,11 +235,11 @@ app.post("/chatmode",async (req,res)=>{
                         "content": prompt
                     }
                 ],
-                temperature:0
+                temperature: 0
             })
         });
         const data = await response.json()
-        console.log("Response from ai: ",data)
+        console.log("Response from ai: ", data)
         res.json({
             response: data.choices[0].message.content
         })
